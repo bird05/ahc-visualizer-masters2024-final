6fc1f11751f45b84b15caaa352e66c38352bf4ce
bird05 <t.junichiro1@gmail.com>
2024-04-17 21:59:13 +0900

    CalcD_defの導入によりCalcScoreSequenceを高速化

--- src/projects/masters_qual/components/display/CalcScore.tsx
+++ src/projects/masters_qual/components/display/CalcScore.tsx
@@ -6,6 +6,7 @@ import { Input_type, Output_type } from "../../types/typeFormat"
 // 関数インポート
 import { is_outer_range } from '../../functions/CommonFunctions';
 
+// 盤面からスコアを計算する関数
 export const CalcScore = function(is_valid:boolean,N:number,v:string[],h:string[],a:number[][],board:number[][]){
   if(!is_valid) return 1;
   if(Number(N) != board.length) return 1;
@@ -13,8 +14,8 @@ export const CalcScore = function(is_valid:boolean,N:number,v:string[],h:string[
   const D = calc_sum_of_squares(N,v,h,board);
   return Math.max(1,Math.round(1000000*getBaseLog(2,D_d/D)));
 }
-
-const calc_sum_of_squares = function(N:number,v:string[],h:string[], board:number[][]){
+// Dを計算する関数
+export const calc_sum_of_squares = function(N:number,v:string[],h:string[], board:number[][]){
   let res:number=0;
   let d:number=0;
 
@@ -38,6 +39,55 @@ const calc_sum_of_squares = function(N:number,v:string[],h:string[], board:numbe
   }
   return res;
 }
+// 前回の計算結果と今回の操作からDを差分計算
+export const CalcD_def = function(
+  is_valid:boolean, N:number, v:string[], h:string[], board:number[][], D:number,
+  x1:number, y1:number, x2:number, y2:number,
+  s:number, d:string, e:string){
+  if(s===0) return D;
+  if(!is_valid) return D;
+  // 移動前の状態を減算
+  D-=get_4squares(v,h,board,x1,y1);
+  D-=get_4squares(v,h,board,x2,y2);
+  // 入替
+  [board[x1][y1],board[x2][y2]]=[board[x2][y2],board[x1][y1]]; // swap
+  // 移動後の状態を加算
+  D+=get_4squares(v,h,board,x1,y1);
+  D+=get_4squares(v,h,board,x2,y2);
+  // 戻す
+  [board[x1][y1],board[x2][y2]]=[board[x2][y2],board[x1][y1]]; // swap
+
+  return D;
+}
+// CalcScore_d_rev
+
+// 4方向との2乗和を求める関数
+const get_4squares = function(v:string[], h:string[], board:number[][], x:number, y:number){
+  let res:number=0;
+  let d:number=0;
+  // L
+  if(y-1>=0 && v[x][y-1]=='0'){ // 範囲内のみ実行,壁なしのみ実行
+    d=board[x][y-1]-board[x][y];
+    res+=d*d;
+  }
+  // R
+  if(y+1<board.length && v[x][y]=='0'){ 
+    d=board[x][y+1]-board[x][y];
+    res+=d*d;
+  }
+  // U
+  if(x-1>=0 && h[x-1][y]=='0'){
+    d=board[x-1][y]-board[x][y];
+    res+=d*d;
+  }
+  // D
+  if(x+1<board.length && h[x][y]=='0'){
+    d=board[x+1][y]-board[x][y];
+    res+=d*d;
+  }
+
+  return res;
+}
 // 底を指定したlogを計算する関数
 function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
@@ -45,15 +95,19 @@ function getBaseLog(x, y) {
 // 各ターンのScoreを計算する関数
 export const CalcScoreSequence = function(input_body:Input_type, output_body:Output_type){
   let board: number[][] = JSON.parse(JSON.stringify(input_body.a)); // 多次元配列のディープコピー
-  const [N,v,h]=[input_body.N,input_body.v,input_body.h];
+  const [N,v,h,a]=[input_body.N,input_body.v,input_body.h,input_body.a];
   const [s,d,e]=[output_body.s,output_body.d,output_body.e]
   let [x1,y1,x2,y2]=[output_body.pi,output_body.pj,output_body.qi,output_body.qj];
 
+  const D_d=calc_sum_of_squares(N,v,h,a);
+  let D=D_d;
   const res = new Array();
 
   for(let turn=0; turn<output_body.s.length; ++turn){
     // 得点計算
-    res[turn]=CalcScore(true,N,v,h,input_body.a,board);
+    // res[turn]=CalcScore(true,N,v,h,input_body.a,board);
+    D=CalcD_def(true,N,v,h,board,D,x1,y1,x2,y2,s[turn],d[turn],e[turn]);
+    res[turn]=Math.max(1,Math.round(1000000*getBaseLog(2,D_d/D)));
     // 入替
     if(s[turn]){
       if(is_outer_range(x1,y1,board.length,board.length)) return res; // 範囲外swapの回避
